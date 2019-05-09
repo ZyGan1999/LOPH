ffe383a39c7ca309a34b6020287281738fe784ca
tube <2017202047@ruc.edu.cn>
2019-05-09 15:09:21 +0800

    Put buttons onto the game layout(with bugs) try more layout

--- app/src/main/java/com/pigeonhunter/loph/GameActivity.java
+++ app/src/main/java/com/pigeonhunter/loph/GameActivity.java
@@ -1,9 +1,16 @@
 package com.pigeonhunter.loph;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class GameActivity extends Activity {
     @Override
@@ -11,11 +18,50 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_game);
+
+        LinearLayout GameLayout =  findViewById(R.id.btLayout);
+
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;         // 屏幕宽度（像素）
+        int height = dm.heightPixels;       // 屏幕高度（像素）
+        float density = dm.density;         // 屏幕密度（0.75 / 1.0 / 1.5）
+        int densityDpi = dm.densityDpi;     // 屏幕密度dpi（120 / 160 / 240）
+        // 屏幕宽度算法:屏幕宽度（像素）/屏幕密度
+        int screenWidth = (int) (width / density);  // 屏幕宽度(dp)
+        int screenHeight = (int) (height / density);// 屏幕高度(dp)
+
+        /*ImageButton ibt = new ImageButton(this );
+        ibt.setScaleX(0.5f);
+        ibt.setScaleY(0.5f);
+        ibt.setImageDrawable(getResources().getDrawable(R.drawable.bbutton));
+        GameLayout.addView(ibt);*/
+
+        for(int i = 0; i < 7;++i){
+            for(int j = 0;j < 7;++j){
+                ImageButton ibt = new ImageButton(this );
+                ibt.setImageDrawable(getResources().getDrawable(R.drawable.bbutton));
+                ibt.setX(screenWidth * i / 7);
+                ibt.setY(screenHeight * j / 7);
+                ibt.setVisibility(View.VISIBLE);
+
+                GameLayout.addView(ibt);
+            }
+        }
+
+
+
+
     }
 
     @Override
     public void onBackPressed() {
         // 拦截返回键
         // TODO: 添加暂停功能
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
+
+
 }
